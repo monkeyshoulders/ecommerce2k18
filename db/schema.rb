d53c1d5c3f5dda4c2f136c1d7c53d2200a3095c5
Leslie Alvarado <lesliemalvarado@gmail.com>
2018-07-13 11:11:09 -0400

    Add Products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_131847) do
+ActiveRecord::Schema.define(version: 2018_07_13_140206) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_07_13_131847) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
